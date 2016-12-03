ecdca1c024c7c51ceddd4f0ff15d6537d8589b08
ph34rt3hcute1 <ph34rt3hcute1@gmail.com>
2016-12-03 00:01:57 -0500

    Added canCast check to cast function.

--- Main.lua
+++ Main.lua
@@ -70,12 +70,15 @@ function(self, event, ...)
 				return false
 			end
 			function cast(Spell,Unit,AoEUnit)
-				CastSpellByName(Spell,Unit)
-				if IsAoEPending() then
-					local X,Y,Z = ObjectPosition(AoEUnit)
-					ClickPosition(X,Y,Z)
+				if canCast(Spell) then
+					CastSpellByName(Spell,Unit)
+					if IsAoEPending() then
+						local X,Y,Z = ObjectPosition(AoEUnit)
+						ClickPosition(X,Y,Z)
+					end
+					return true
 				end
-				return true	
+				return false	
 			end
 		-- Rotation
 	  		if ObjectExists("target") and not UnitIsFriend("target","player") and not UnitIsDeadOrGhost("target") then
@@ -86,23 +89,19 @@ function(self, event, ...)
 
 		  	-- Fiery Brand
 		  		-- actions+=/fiery_brand,if=buff.demon_spikes.down&buff.metamorphosis.down
-		  		if canCast("Fiery Brand") then
-		  			if not UnitAura("player","Fiery Brand") and not UnitAura("player","Metamorphosis") then
-		  				cast("Fiery Brand","target");
-		  				return
-					end
+	  			if not UnitAura("player","Fiery Brand") and not UnitAura("player","Metamorphosis") then
+	  				cast("Fiery Brand","target");
+	  				return
 				end
 			-- Demon Spikes
 				-- actions+=/demon_spikes,if=charges=2|buff.demon_spikes.down&!dot.fiery_brand.ticking&buff.metamorphosis.down
-				if canCast("Demon Spikes") then
-					if select(1,GetSpellCharges("Demon Spikes")) == 2 then
-						cast("Demon Spikes","player");
-		  				return
-					end
+				if select(1,GetSpellCharges("Demon Spikes")) == 2 then
+					cast("Demon Spikes","player");
+	  				return
 				end
 			-- Infernal Strike
 				-- actions+=/infernal_strike,if=!sigil_placed&!in_flight&remains-travel_time-delay<0.3*duration&(!artifact.fiery_demise.enabled|(max_charges-charges_fractional)*recharge_time<cooldown.fiery_brand.remains+5)&(cooldown.sigil_of_flame.remains>7|charges=2)
-				if canCast("Infernal Strike") and canCast("Shear") then
+				if canCast("Shear") then
 					if select(1,GetSpellCharges("Infernal Strike")) == 2 then
 						cast("Infernal Strike","player","player");
 		  				return
@@ -110,110 +109,79 @@ function(self, event, ...)
 				end
 			-- Spirit Bomb
 				-- actions+=/spirit_bomb,if=debuff.frailty.down
-				if canCast("Spirit Bomb") then
-					if not UnitAuras("target","Frailty","player") then
-						cast("Spirit Bomb","target");
-		  				return
-					end
+				if not UnitAuras("target","Frailty","player") then
+					cast("Spirit Bomb","target");
+	  				return
 				end
 			-- Soul Carver
 				-- actions+=/soul_carver,if=dot.fiery_brand.ticking
-				if canCast("Soul Carver") then
-					if UnitAuras("target","Fiery Brand","player") ~= nil then
-						cast("Soul Carver","target");
-		  				return
-					end
+				if UnitAuras("target","Fiery Brand","player") ~= nil then
+					cast("Soul Carver","target");
+	  				return
 				end
 			-- Immolation
 				-- actions+=/immolation_aura,if=pain<=80
-				if canCast("Immolation Aura") then
-					if pain <= 80 then
-						cast("Immolation Aura","player");
-		  				return
-					end
+				if pain <= 80 then
+					cast("Immolation Aura","player");
+	  				return
 				end
 			-- Felblade
 				-- actions+=/felblade,if=pain<=70
-				if canCast("Felblade") then
-					if pain <= 70 then
-						cast("Felblade","target");
-		  				return
-					end
+				if pain <= 70 then
+					cast("Felblade","target");
+	  				return
 				end
 			-- Soul Barrier
 				-- actions+=/soul_barrier
-				if canCast("Soul Barrier") then
-					cast("Soul Barrier","player");
-	  				return
-				end
+				cast("Soul Barrier","player")
 			-- Soul Cleave
 				-- actions+=/soul_cleave,if=soul_fragments=5
-				if canCast("Soul Cleave") then
-					if getAuraStacks("player","Soul Fragments") == 5 then
-						cast("Soul Cleave","target");
-		  				return
-					end
+				if getAuraStacks("player","Soul Fragments") == 5 then
+					cast("Soul Cleave","target");
+	  				return
 				end
 			-- Metamorphosis
 				-- actions+=/metamorphosis,if=buff.demon_spikes.down&!dot.fiery_brand.ticking&buff.metamorphosis.down&incoming_damage_5s>health.max*0.70
-				if canCast("Metamorphosis") then
-					if not UnitAuras("player","Demon Spikes") and not UnitAuras("target","Fiery Brand","player") and not UnitAuras("player","Metamorphosis") and php < 30 then
-						cast("Metamorphosis","player");
-		  				return
-					end
+				if not UnitAuras("player","Demon Spikes") and not UnitAuras("target","Fiery Brand","player") and not UnitAuras("player","Metamorphosis") and php < 30 then
+					cast("Metamorphosis","player");
+	  				return
 				end
 			-- Fel Devastation
 				-- actions+=/fel_devastation,if=incoming_damage_5s>health.max*0.70
-				if canCast("Fel Devastation") then
-					if php < 30 then
-						cast("Fel Devastation","player");
-		  				return
-					end
+				if php < 30 then
+					cast("Fel Devastation","player");
+	  				return
 				end
 			-- Soul Cleave
 				-- actions+=/soul_cleave,if=incoming_damage_5s>=health.max*0.70
-				if canCast("Soul Cleave") then
-					if php < 30 then
-						cast("Soul Cleave","target");
-		  				return
-					end
+				if php < 30 then
+					cast("Soul Cleave","target");
+	  				return
 				end
 			-- Fel Eruption
 				-- actions+=/fel_eruption
-				if canCast("Fel Eruption") then
-					cast("Fel Eruption","player");
-	  				return
-				end
+				cast("Fel Eruption","player")
 			-- Sigil of Flame
 				-- actions+=/sigil_of_flame,if=remains-delay<=0.3*duration
-				if canCast("Sigil of Flame") then
-					if getAuraRemain("target","Sigil of Flame","player") - 1 <= 0.3 * getAuraDuration("target","Sigil of Flame","player") then
-						cast("Sigil of Flame","player","target");
-		  				return
-					end
+				if getAuraRemain("target","Sigil of Flame","player") - 1 <= 0.3 * getAuraDuration("target","Sigil of Flame","player") then
+					cast("Sigil of Flame","target","target");
+	  				return
 				end
 			-- Fracture
 				-- actions+=/fracture,if=pain>=80&soul_fragments<4&incoming_damage_4s<=health.max*0.20
-				if canCast("Fracture") then
-					if pain >= 80 and getAuraStacks("player","Soul Fragments") < 4 and php > 80 then
-						cast("Fracture","target");
-		  				return
-					end
+				if pain >= 80 and getAuraStacks("player","Soul Fragments") < 4 and php > 80 then
+					cast("Fracture","target");
+	  				return
 				end
 			-- Soul Cleave
 				-- actions+=/soul_cleave,if=pain>=80
-				if canCast("Soul Cleave") then
-					if pain >= 80 then
-						cast("Soul Cleave","target");
-		  				return
-					end
+				if pain >= 80 then
+					cast("Soul Cleave","target");
+	  				return
 				end
 			-- Shear
 				-- actions+=/shear
-				if canCast("Shear") then
-					cast("Shear","target");
-	  				return
-				end
+				cast("Shear","target");
 			end
 	        timer = 0
 	    end
